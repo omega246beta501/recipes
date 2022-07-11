438624cb8b81d69c58a8397ddaad1ab4dd8d4e9a
rubenoo <ruben_ayg@hotmail.com>
2022-07-11 21:36:09 +0000

    Now its possible to edit recipes from the menu

--- resources/views/components/menu/table.blade.php
+++ resources/views/components/menu/table.blade.php
@@ -17,7 +17,7 @@
     @foreach($recipes as $recipe)
     <tr>
         <th>{{ $recipe->id }}</th>
-        <th><a href="{{ str_replace('{id}', $recipe->id, RecipeRoutes::RECIPE) }}">{{ $recipe->name }}</a></th>
+        <th><a onclick="openModal('updateModal', {{ $recipe->id }})">{{ $recipe->name }}</a></th>
         @if($isMenuSet)
         <th><input class="form-check-input keeped" type="checkbox" value="" id="{{ $recipe->id }}" checked disabled></th>
         @else
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,5 +1,6 @@
 @php
 use App\Data\Routes\MenuRoutes;
+use App\Data\Routes\RecipeRoutes;
 @endphp
 <x-layout>
     <x-slot:title>Menú</x-slot>
@@ -77,6 +78,14 @@ use App\Data\Routes\MenuRoutes;
                 <div class="col"></div>
             </div>
             @endif
+        </div>
+        <div class="modalContainer">
+            <x-elements.modal>
+                <x-slot:modalId>updateModal</x-slot:modalId>
+                <x-slot:title>Actualizar Receta</x-slot:title>
+                <!-- Al hacer una peticion al controller, este renderizara el form -->
+                <div id="updateRecipeForm"></div>
+            </x-elements.modal>
         </div>
         <script>
             $(document).ready(function() {
@@ -202,6 +211,34 @@ use App\Data\Routes\MenuRoutes;
                         location.reload();
                     });
                 }
+            }
+
+            function openModal(modalId, recipeId) {
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "{{ RecipeRoutes::UPDATE_VIEW }}".replace("{id}", recipeId),
+                    "method": "GET",
+                    "headers": {
+                        "cache-control": "no-cache",
+                        "postman-token": "beeffe31-037f-448b-b45a-382e3b7c8e1c"
+                    }
+                }
+
+                $.ajax(settings).done(function(response) {
+                    $("#updateRecipeForm").html(response);
+                    // Select2 situado en el formulario que general el controller. Metodo UpdateView
+                    $('#updateSelec2Categories').select2({
+                        width: 'resolve',
+                        placeholder: "Categorías a incluir (Opcional)",
+                        dropdownParent: $('#' + modalId)
+                    });
+
+                    $('#' + modalId).modal('show');
+                    $('#' + modalId).addClass('xyz-in');
+                    $('#' + modalId).removeClass('xyz-out');
+                });
+
             }
         </script>
 </x-layout>
