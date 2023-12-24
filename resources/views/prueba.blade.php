3aa7ec324d86b89ef2de44bce33813236f766e38
rubenoo <ruben_ayg@hotmail.com>
2023-12-24 17:05:33 +0000

    Anyadida clase para cuando no haya fotos

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -94,10 +94,22 @@
             width: 100%;
         }
 
-        img {
-            max-width: 80%;
+        .image {
             border-radius: 50%;
-            height: 40px;
+            height: 48px;
+            width: 48px;
+        }
+
+        .no-image {
+            height: 48px;
+            width: 48px;
+            border-radius: 50%;
+            background-color: #F6F7FA;
+            fill: #E4E6EA;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex: 0 0 auto;
         }
 
         .fixed-div {
@@ -298,7 +310,12 @@
             @foreach ($recipes as $recipe)
             <div class="row card-element">
                 <div class="col-xs-2" style="text-align: center;">
-                    <img src="http://192.168.0.177/storage/img_chania.jpg">
+                    <div class="no-image">
+                        <svg viewBox="0 0 24 24" width="24" height="24">
+                            <path d="m16.736 9.252-3.982-5.876A.907.907 0 0 0 12 3a.892.892 0 0 0-.755.385L7.264 9.252H2.909c-.5 0-.909.403-.909.896 0 .08.01.16.036.241l2.31 8.303C4.554 19.445 5.254 20 6.09 20h11.818c.837 0 1.537-.555 1.755-1.308l2.309-8.303.027-.241a.905.905 0 0 0-.91-.896h-4.354Zm-7.463 0L12 5.31l2.727 3.94H9.273ZM12 16.417c-1 0-1.818-.806-1.818-1.791 0-.985.818-1.791 1.818-1.791s1.818.806 1.818 1.79c0 .986-.818 1.792-1.818 1.792Z"></path>
+                        </svg>
+                    </div>
+                    <!-- <img class="image" src="http://192.168.0.177/storage/img_chania.jpg"> -->
                 </div>
                 <div class="col-xs-7" style="padding-left: 5px;">
                     <span>{{ $recipe->name }}</span>
