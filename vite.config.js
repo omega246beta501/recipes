152dc1b0aaaf4eae743f01ad28e080a416ca7eb5
rubenoo <ruben_ayg@hotmail.com>
2023-12-29 23:58:07 +0000

    Anyadida ip al .env

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import dotenv from 'dotenv';
 
+dotenv.config();
 export default defineConfig({
     server: {
         host: '0.0.0.0',
         hmr: {
-            host: 'localhost'
+            host: process.env.VITE_BASE_IP
         }
     },
     plugins: [
