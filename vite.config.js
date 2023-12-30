397391077b7b933b34d1c6aaadc2aa60fc8208ed
rubenoo <ruben_ayg@hotmail.com>
2023-12-30 00:30:58 +0000

    me rindo

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import dotenv from 'dotenv';
 
-dotenv.config();
 export default defineConfig({
     server: {
         host: '0.0.0.0',
         hmr: {
-            host: process.env.VITE_BASE_IP
+            host: 'localhost'
         }
     },
     plugins: [
